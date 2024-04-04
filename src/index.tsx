8f9eac81d10419f428e91fdf65446ebd5cd7bc67
byron <47872200+byeoon@users.noreply.github.com>
2024-04-04 02:05:27 +0000

    added toast show

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
 import { getByProps } from 'enmity/metro';
+import { Toasts } from 'enmity/metro/common';
+import {getIDByName} from "enmity/api/assets";
 import { create } from 'enmity/patcher';
 import manifest from '../manifest.json';
 
 const Patcher = create('ActionSheetFinder');
 const LazyActionSheet = getByProps("openLazy", "hideActionSheet");
+
+function SheetOutput(text) {
+   console.log("[ActionSheetFinder] Found ActionSheet: " + text); 
+
+   Toasts.open({
+      content: "[ActionSheetFinder] Found ActionSheet: " + text,
+      icon: getIDByName('Check')
+   });
+}
+
+
 const ActionSheetFinder: Plugin = {
    ...manifest,
 
    onStart() {
       console.log("[ActionSheetFinder] Hello world!")
       const unpatchOpenLazy = Patcher.before(LazyActionSheet, 'openLazy', (_, [component, key]) => {
-         return console.log("[ActionSheetFinder] Found ActionSheet: " + key);
+         return SheetOutput(key); 
          unpatchOpenLazy();
       });
    },
